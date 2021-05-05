10c3a61a1e7dfa221e5372e8c9ec9157d9a0f571
kyngmn <79073979+kyngmn@users.noreply.github.com>
2021-05-05 16:14:24 +0900

    elements 80% 완료

--- src/elements/Text.js
+++ src/elements/Text.js
@@ -2,110 +2,80 @@ import React from "react";
 import styled from "styled-components";
 
 const Text = (props) => {
-  const { children, onClick, color, size, weight, flexGrow, marginB, marginT, textAlign, h1, h2, h3, h4, h5, h6, h7, Title, subTitle, note, price, priceSm } = props;
-  // const weightStyles = { 600: bold, 500: medium };
+  const { children, onClick, color, size, flexGrow, marginB, marginT, textAlign, h1, h2, h3, h4, body, subBody, price } = props;
+  const styles = { flexGrow: flexGrow, marginB: marginB, marginT: marginT, textAlign: textAlign };
 
-  // h1, h2, h3, h4, h5, h6 디자인 시스템에 맞춘 폰트 크기
+  // h1, h2, h3, SubHeading(h4) 디자인 시스템에 맞춘 폰트
+  // h1 : 메인배너
   if (h1) {
     return (
-      <TextBox size="24px" weight={weight} marginB={marginB}>
+      <TextBox full size="45px" bold onClick={onClick} {...styles}>
         {children}
       </TextBox>
     );
   }
+  // h2 : 카테고리 제목 / 상세페이지 상품명
   if (h2) {
     return (
-      <TextBox size="22px" weight={weight} marginB={marginB}>
+      <TextBox full size="35px" bold onClick={onClick} {...styles}>
         {children}
       </TextBox>
     );
   }
+  // h3 : 상세페이지 카테고리 제목 / 버튼 폰트 크기와 동일
   if (h3) {
     return (
-      <TextBox size="20px" weight={weight} marginB={marginB}>
+      <TextBox full size="20px" bold onClick={onClick} {...styles}>
         {children}
       </TextBox>
     );
   }
+  // sub h4: 가격명
   if (h4) {
     return (
-      <TextBox size="18px" weight={weight} marginB={marginB}>
-        {children}
-      </TextBox>
-    );
-  }
-  if (h5) {
-    return (
-      <TextBox size="16px" weight={weight} marginB={marginB}>
-        {children}
-      </TextBox>
-    );
-  }
-  if (h6) {
-    return (
-      <TextBox size="14px" weight={weight} marginB={marginB}>
-        {children}
-      </TextBox>
-    );
-  }
-  if (h7) {
-    return (
-      <TextBox size="12px" weight={weight} marginB={marginB}>
+      <TextBox full size="16px" onClick={onClick} {...styles}>
         {children}
       </TextBox>
     );
   }
 
-  // 가격
+  // 조회수, 카테고리 설명
+  if (body) {
+    return (
+      <TextBox onClick={onClick} {...styles}>
+        {children}
+      </TextBox>
+    );
+  }
+  // 조회수, 카테고리 설명
+  if (subBody) {
+    return (
+      <TextBox size="12px" onClick={onClick} {...styles}>
+        {children}
+      </TextBox>
+    );
+  }
+
+  // 가격, 디자인 시스템에는 따로 없으나 코드에서 따로 뺌
   if (price) {
     return (
-      <TextBox size="24px" weight="700" onClick={onClick} color={color} flexGrow={flexGrow} textAlign="right" marginB="1%" marginT="1%" {...props}>
+      <TextBox size="20px" onClick={onClick} {...styles}>
         {children}
       </TextBox>
     );
-  }
-
-  // 제목
-  if (Title) {
-    return (
-      <TextBox full size="24px" weight="700" onClick={onClick} color={color} flexGrow={flexGrow} textAlign={textAlign} marginB="1%" marginT="1%" {...props}>
-        {children}
-      </TextBox>
-    );
-  }
-
-  // 부제목
-  if (subTitle) {
-    return (
-      <TextBox size="14px" weight="600" onClick={onClick} color="#9e9e9e" full flexGrow={flexGrow} textAlign={textAlign} marginB="1%" marginT={marginT} {...props}>
-        {children}
-      </TextBox>
-    );
-  }
-
-  // 주석
-  if (note) {
-    return (
-      <TextBox size="12px" weight="600" onClick={onClick} color="#808080" flexGrow={flexGrow} textAlign={textAlign} marginB="1%" marginT={marginT} {...props}>
-        {children}
-      </TextBox>
-    );
-  }
-
-  // props
-  if (children) {
-    return <TextBox {...props}>{children}</TextBox>;
   }
 };
 
+// body 기준 default임
 Text.defaultprops = {
   color: false,
-  size: "16px",
-  weight: "500",
+  size: "14px",
+  weight: "400",
   flexGrow: false,
   marginB: false,
   marginT: false,
   textAlign: "left",
+  children: "",
   onClick: () => {},
 };
 
@@ -116,14 +86,16 @@ const TextBox = styled.div`
   height: fit-content;
   margin-bottom: ${(props) => (props.marginB ? props.marginB : "")};
   margin-top: ${(props) => (props.marginT ? props.marginT : "")};
-  /* border-bottom: 2px solid grey; */
 
   color: ${(props) => (props.color ? props.color : "")};
 
   font-size: ${(props) => (props.size ? props.size : "")};
-  font-weight: ${(props) => (props.weight ? props.weight : "")};
+  font-weight: ${(props) => (props.bold ? "700" : props.light ? "300" : "")};
   line-height: ${(props) => (props.flexGrow ? props.flexGrow : "")};
   ${(props) => (props.textAlign ? `text-align:${props.textAlign};` : "")}
+
+  word-spacing:-1.5px;
+  letter-spacing: 0.25px;
 `;
 
 export default Text;
--- src/elements/index.js
+++ src/elements/index.js
@@ -2,10 +2,10 @@ import Button from "./Button";
 import Grid from "./Grid";
 import Image from "./Image";
 import Input from "./Input";
-import Spinner from "./Spinner";
 import Text from "./Text";
 import Line from "./Line";
 import Tag from "./Tag";
 import Modal from "./Modal";
+import Profile from "./Profile";
 
-export { Button, Grid, Image, Input, Spinner, Text, Line, Tag, Modal };
+export { Button, Grid, Image, Input, Text, Line, Tag, Modal, Profile };
