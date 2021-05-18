ac00ac8e978e8b3c5f077b46e3cb5aea624fdb61
chldydgus777 <chldydgus777@naver.com>
2021-05-18 18:08:52 +0900

    y

--- src/components/my_page_1/Myinfo.js
+++ src/components/my_page_1/Myinfo.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import styled from 'styled-components'
 import {Input, Line, Button, Tag, Modal, Text, Profile } from "elements/";
 
+import { useSelector, useDispatch } from "react-redux";
+
+import { actionCreators as mypageActions } from "redux/modules/mypage";
+import { actionCreators as likeActions } from "redux/modules/like";
+
 const Myinfo = () => {
+    const dispatch = useDispatch();
+    const access_token = localStorage.getItem("access_token");
+    const _user = useSelector((state) => state.mypage.user);;
+    const userInfo = useSelector((state) => state.mypage.user_info);
+    console.log(userInfo,"dddd")
+    console.log(_user);
+
+    useEffect(() => {
+    if (access_token) {
+        dispatch(mypageActions.setProfileAPI());
+        dispatch(mypageActions.setInfoAPI());
+        dispatch(mypageActions.setMystoreAPI());
+        dispatch(likeActions.getMyLikeListAPI());
+    }
+    }, []);
+
     return (
         <Grid>
             <Head>
@@ -12,39 +33,35 @@ const Myinfo = () => {
                 <Modify>수정하기</Modify>
             </Head>
             <InfoBox>
-                <Info>
-                    <Title>
-                        닉네임
-                    </Title>
-                    <Blank>
-                        <input/>
-                    </Blank>
-                </Info>
-                <Info>
-                    <Title>
-                        아이디
-                    </Title>
-                    <Blank>
-                        <input/>
-                    </Blank>
-                </Info>
-                <Info>
-                    <Title>
-                        비밀번호
-                    </Title>
-                    
-                    <Blank>
-                        <input type="password"/>    
-                    </Blank>
-                </Info>
-                <Info>
-                    <Title>
-                        전화번호
-                    </Title>
-                    <Blank>
-                        <input/>
-                    </Blank>
-                </Info>
+                <InnerBox>
+                    <Info>
+                        <Title>
+                            닉네임
+                        </Title>
+                        <Blank disabled placeholder={`${userInfo.nickname}`}>
+                        </Blank>
+                    </Info>
+                    <Info>
+                        <Title>
+                            아이디
+                        </Title>
+                        <Blank disabled placeholder={`${userInfo.email}`}>
+                        </Blank>
+                    </Info>
+                    <Info>
+                        <Title>
+                            비밀번호
+                        </Title>
+                        <Blank disabled type="password">
+                        </Blank>
+                    </Info>
+                    <Info>
+                        <Title>
+                            전화번호
+                        </Title>
+                        <Blank disabled placeholder={`${userInfo.number}`}/>
+                    </Info>
+                </InnerBox>
             </InfoBox>
         </Grid>
     )
@@ -64,34 +81,49 @@ margin : 25px auto 19px ;
 const Modify = styled.div`
 font-size : 16px;
 margin : 20px;
+cursor : pointer;
 `;
 const InfoBox = styled.div`
+display : flex;
+align-items : center;
+justify-content : center;
 width : 1030px;
 background : #f8f8f8;
 border-radius: 16px;
+box-sizing : border-box;
+height : 326px;
+`;
+const InnerBox = styled.div`
+display : flex;
+flex-direction: column;
+justify-content : space-between;
+box-sizing : border-box;
+height : 80%;
+width : 90%;
 `;
 
 const Info = styled.div`
 display :flex;
+max-width : 1030px;
+height : 50px;
+justify-content: space-between;
+
 `;
 
 const Title = styled.p`
 font-size : 16px;
-margin : 45px 70px 23.5px 55px;
+height : 50px;
+padding : 12.5px;
 `;
 
-const Blank = styled.p`
-margin-top : 25px;
-& > input {
-    border : 1px solid #ffffff;
+const Blank = styled.input`
+    border : 1px solid #eee;
     border-radius : 8px;
     width : 806px;
     height : 50px;
     font-size : 16px;
-}
-& : focus {
-outline:none;
-}
+    outline:none;
+
 
 `;
 
