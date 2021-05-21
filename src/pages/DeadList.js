5fca5ff7bc820d77286f3e33582a3c201c7e6d86
kyngmn <79073979+kyngmn@users.noreply.github.com>
2021-05-22 03:13:19 +0900

    상품 더보기s

--- src/pages/DeadList.js
+++ src/pages/DeadList.js
@@ -6,6 +6,7 @@ import { actionCreators as postActions } from "redux/modules/post";
 import { useDispatch, useSelector } from "react-redux";
 
 import { useMediaQuery } from "react-responsive";
+import { Grid, Input, Line, Button, Tag, Modal, Text, Profile } from "elements/";
 
 import PostCard from "components/PostCard";
 import PostCardMobile from "components/PostCardMobile";
@@ -37,44 +38,52 @@ const DeadList = () => {
     <>
       <Desktop>
         <DeadContainer>
-          <span>마감임박 리스트</span>
-          {!_deadline_product ? (
-            <div style={{ margin: "100px auto", color: "#c0c0c0 ", fontSize: "20px" }}>현재 마감임박 상품이 없습니다</div>
-          ) : (
-            _deadline_product.map((k, index) => {
-              return (
-                <PostCard
-                  key={index}
-                  {...k}
-                  _onClick={() => {
-                    history.push(`product/detail/${k._id}`);
-                  }}
-                />
-              );
-            })
-          )}
+          <SectionTitle>
+            <Text h2>마감임박 리스트</Text>
+          </SectionTitle>
+          <CardWrap>
+            {!_deadline_product ? (
+              <div style={{ margin: "100px auto", color: "#c0c0c0 ", fontSize: "20px" }}>현재 마감임박 상품이 없습니다</div>
+            ) : (
+              _deadline_product.map((k, index) => {
+                return (
+                  <PostCard
+                    key={index}
+                    {...k}
+                    _onClick={() => {
+                      history.push(`product/detail/${k._id}`);
+                    }}
+                  />
+                );
+              })
+            )}
+          </CardWrap>
         </DeadContainer>
       </Desktop>
       <Tablet>Tablet</Tablet>
 
       <Mobile>
         <DeadContainer>
-          <span>마감임박 리스트</span>
-          {!_deadline_product ? (
-            <div style={{ margin: "100px auto", color: "#c0c0c0 ", fontSize: "20px" }}>현재 마감임박 상품이 없습니다</div>
-          ) : (
-            _deadline_product.map((k, index) => {
-              return (
-                <PostCardMobile
-                  key={index}
-                  {...k}
-                  _onClick={() => {
-                    history.push(`product/detail/${k._id}`);
-                  }}
-                />
-              );
-            })
-          )}
+          <SectionTitle>
+            <Text h2>마감임박 리스트</Text>
+          </SectionTitle>
+          <CardWrap>
+            {!_deadline_product ? (
+              <div style={{ margin: "100px auto", color: "#c0c0c0 ", fontSize: "20px" }}>현재 마감임박 상품이 없습니다</div>
+            ) : (
+              _deadline_product.map((k, index) => {
+                return (
+                  <PostCardMobile
+                    key={index}
+                    {...k}
+                    _onClick={() => {
+                      history.push(`product/detail/${k._id}`);
+                    }}
+                  />
+                );
+              })
+            )}
+          </CardWrap>
         </DeadContainer>
       </Mobile>
     </>
@@ -82,16 +91,34 @@ const DeadList = () => {
 };
 
 const DeadContainer = styled.div`
+  margin: 160px auto 20px auto;
+
   display: flex;
-  justify-content: flex-start;
+  flex-direction: column;
+
   flex-wrap: wrap;
   height: 100%;
+  gap: 33px;
 
   @media only screen and (max-width: 767px) {
-    display: flex;
-    justify-content: flex-start;
-    flex-wrap: wrap;
-    height: 100%;
+    margin: 130px auto 20px auto;
+  }
+`;
+
+const SectionTitle = styled.div`
+  margin: 0 auto;
+`;
+
+const CardWrap = styled.div`
+  justify-content: flex-start;
+  display: flex;
+  justify-content: center;
+  max-width: 1030px;
+  height: 100%;
+  gap: 10px;
+
+  @media only screen and (max-width: 767px) {
+    flex-direction: column;
   }
 `;
 
